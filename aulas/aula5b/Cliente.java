6821850a106d9e13854d8f0231c9cbe11e2d0848
Rodrigo Costa <57047212+rfcosta85@users.noreply.github.com>
2020-09-24 19:13:36 -0300

    Update

--- aulas/aula5b/Cliente.java
+++ aulas/aula5b/Cliente.java
@@ -8,14 +8,25 @@ public class Cliente
         
         ContaBanco cliente1 = new ContaBanco();
 
-        cliente1.abrirConta("CC");        
-        cliente1.depositar(150);
-        System.out.println("\nSaldo da conta corrente é de: " + cliente1.getSaldo() + "\n");
-        cliente1.sacar(40);
-        System.out.println("\nSaldo da conta corrente é de: " + cliente1.getSaldo() + "\n");
+        cliente1.abrirConta("CC");
+        cliente1.setNumeroConta(28931);
+        cliente1.setDono("Jubileu");        
+        cliente1.depositar(300);
+        System.out.println("\nNome do Cliente: " + cliente1.getDono());
+        System.out.println("\nNumero da Conta: " + cliente1.getNumeroConta());        
         cliente1.pagarMensalidade();
         System.out.println("\nSaldo da conta corrente é de: " + cliente1.getSaldo() + "\n");
-        //cliente1.fecharConta();
+        
+        ContaBanco cliente2 = new ContaBanco();
+
+        cliente2.abrirConta("CP");
+        cliente2.setNumeroConta(18994);
+        cliente2.setDono("Creuza");
+        cliente2.depositar(500);
+        System.out.println("\nNome do Cliente: " + cliente2.getDono());
+        System.out.println("\nNúmero da Conta: " + cliente2.getNumeroConta());
+        cliente2.pagarMensalidade();
+        System.out.println("\nSaldo da conta corrente é de: " + cliente2.getSaldo() + "\n");
 
     }
 
